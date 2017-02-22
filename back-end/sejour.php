10bd7ab152b64add03a2284aa6a74f6937985df5
simplon-mariyamH <mariyam.hamdaoui@gmail.com>
2017-02-22 23:09:31 +0100

    fichier telechargeable dans sejour

--- back-end/sejour.php
+++ back-end/sejour.php
@@ -13,9 +13,8 @@
 		<h1>NOS SEJOURS</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque et massa sed facilisis. Quisque pulvinar leo ac urna sollicitudin, in elementum metus ultrices. Proin consequat pretium mollis. In malesuada auctor nulla, vel ultrices nibh semper sed. Donec tristique tincidunt augue quis dapibus. Phasellus nec lobortis velit. Phasellus viverra augue et pretium sodales. Nullam lectus enim, dapibus a hendrerit vel, bibendum at lorem.</p>
 		<p>Donec sed nisi volutpat nisi sodales consequat eget eu velit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus sapien libero, faucibus nec fermentum eget, viverra ut magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper dictum erat sit amet aliquam. Donec ut egestas elit. Nulla faucibus eget justo id varius. Cras porta nibh et congue ultricies. Proin accumsan feugiat sapien consequat sodales. Sed dignissim sapien porta, auctor justo sed, ultricies ligula. Fusce ultricies sem justo.</p>
-		<p>Donec auctor, mi non consequat tincidunt, metus risus dignissim ipsum, a facilisis arcu odio vel quam. Fusce lacinia porttitor enim, eu laoreet quam consectetur et. Etiam lobortis pellentesque massa, vel dapibus velit tincidunt quis. Suspendisse tempor magna rutrum ornare commodo. Donec aliquet sodales porta. Nullam et blandit nulla, vel bibendum eros. Vivamus a libero nisl. Maecenas vehicula eu tortor sed convallis. Integer in suscipit felis. Sed eget sem felis. Sed ornare eleifend iaculis. Pellentesque volutpat suscipit sapien vel fringilla. Fusce rhoncus risus mauris, at dignissim arcu mattis quis. In fermentum lacus felis, et rutrum erat laoreet non. Etiam tempor sem in tristique eleifend. Etiam porta dapibus blandit.
-
-</p>
+		<p>Plus d'info télécharger : <a href="projeteducatifascjl.html">Projet éducatif de l’Association sportif et culturelle des jeunes Lyonnais</a>
+		</p>
 	</div>
 	<div class="contenu">
 		<div class='sejour'>
